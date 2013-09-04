a3d675802d61081513e1b214524d4b2cfb03be12
ChrisWongAtCUHK <wc38.wong@gmail.com>
2013-09-04 10:04:21 +0800

    Revert "Separates an object¡¦s interface from its implementation"
    This reverts commit 84fdd8c9aa1bdb7044be187626770bba2e3e7f31.

--- Bridge/cpp/CivilianTime.cpp
+++
@@ -1,5 +0,0 @@
-#include "CivilianTime.h"
-
-CivilianTime::CivilianTime(int hr, int min, int pm) {
-	imp_ = new CivilianTimeImp(hr, min, pm);
-}
--- Bridge/cpp/ZuluTime.cpp
+++
@@ -1,5 +0,0 @@
-#include "ZuluTime.h"
-
-ZuluTime::ZuluTime(int hr, int min, int zone) {
-	imp_ = new ZuluTimeImp(hr, min, zone);
-}
