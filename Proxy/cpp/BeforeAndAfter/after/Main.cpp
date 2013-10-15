1fe2f1cbdf9ae9f166bd9aec1b4a84b7977c2a78
Master C <hfyin@cse.cuhk.edu.hk>
2013-10-15 17:09:46 +0800

    An object representing another object

--- Proxy/cpp/BeforeAndAfter/after/Main.cpp
+++ Proxy/cpp/BeforeAndAfter/after/Main.cpp
@@ -2,24 +2,23 @@
 
 using namespace std;
 
-
+// Real class
 class RealImage
 {
 	int m_id;
 	public:
-	RealImage(int i)
-	{
-		m_id = i;
-		cout << "   $$ ctor: " << m_id << '\n';
-	}
-	~RealImage()
-	{
-		cout << "   dtor: " << m_id << '\n';
-	}
-	void draw()
-	{
-		cout << "   drawing image " << m_id << '\n';
-	}
+		// constructor
+		RealImage(int i){
+			m_id = i;
+			cout << "   $$ ctor: " << m_id << '\n';
+		}
+		// destructor
+		~RealImage(){
+			cout << "   dtor: " << m_id << '\n';
+		}
+		void draw(){
+			cout << "   drawing image " << m_id << '\n';
+		}
 };
 
 // 1. Design an "extra level of indirection" wrapper class
@@ -30,38 +29,40 @@ class Image
 	int m_id;
 	static int s_next;
 	public:
-	Image()
-	{
-		m_id = s_next++;
-		// 3. Initialized to null
-		m_the_real_thing = 0;
-	}
-	~Image()
-	{
-		delete m_the_real_thing;
-	}
-	void draw()
-	{
-		// 4. When a request comes in, the real object is
-		//    created "on first use"
-		if (!m_the_real_thing)
-			m_the_real_thing = new RealImage(m_id);
-		// 5. The request is always delegated
-		m_the_real_thing->draw();
-	}
+		// constructor
+		Image(){
+			m_id = s_next++;
+			// 3. Initialized to null
+			m_the_real_thing = 0;
+		}
+		// destructor
+		~Image(){
+			delete m_the_real_thing;
+		}
+		void draw(){
+			// 4. When a request comes in, the real object is
+			//    created "on first use"
+			if (!m_the_real_thing)
+				m_the_real_thing = new RealImage(m_id);
+			// 5. The request is always delegated
+			m_the_real_thing->draw();
+		}
 };
 int Image::s_next = 1;
 
-int main()
-{
+/*
+ * An object representing another object
+ */
+int main(int argc, char *argv[]){
 	Image images[5];
-
-	for (int i; true;)
-	{
+	int i;
+	while(true){
 		cout << "Exit[0], Image[1-5]: ";
 		cin >> i;
 		if (i == 0)
 			break;
 		images[i - 1].draw();
 	}
+
+	return 0;
 }
--- Proxy/cpp/BeforeAndAfter/before/Main.cpp
+++ Proxy/cpp/BeforeAndAfter/before/Main.cpp
@@ -2,38 +2,38 @@
 
 using namespace std;
 
-
 class Image
 {
 	int m_id;
 	static int s_next;
 	public:
-	Image()
-	{
-		m_id = s_next++;
-		cout << "   $$ ctor: " << m_id << '\n';
-	}
-	~Image()
-	{
-		cout << "   dtor: " << m_id << '\n';
-	}
-	void draw()
-	{
-		cout << "   drawing image " << m_id << '\n';
-	}
+		// constructor
+		Image(){
+			m_id = s_next++;
+			cout << "   $$ ctor: " << m_id << '\n';
+		}
+		// destructor
+		~Image(){
+			cout << "   dtor: " << m_id << '\n';
+		}
+		void draw(){
+			cout << "   drawing image " << m_id << '\n';
+		}
 };
 int Image::s_next = 1;
 
-int main()
-{
+int main(int argc, char *argv[]){
+	cout << "A" << endl;
 	Image images[5];
-
-	for (int i; true;)
-	{
+	cout << "B" << endl;
+	int i;
+	while(true){
 		cout << "Exit[0], Image[1-5]: ";
 		cin >> i;
 		if (i == 0)
 			break;
 		images[i - 1].draw();
 	}
+
+	return 0;
 }
