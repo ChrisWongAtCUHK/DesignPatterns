3e6f9c386d5bed859d466d34ce24bd72ff5b855d
Chris Wong <wc38.wong@gmail.com>
2014-01-23 11:26:50 +0800

    Remove an element

--- Adapter/cpp/java/src/dp/adapter/demo/ExecuteAdapterDemo.java
+++ Adapter/cpp/java/src/dp/adapter/demo/ExecuteAdapterDemo.java
@@ -7,11 +7,10 @@ public class ExecuteAdapterDemo{
 	private static final String EXEC_PACKAGE = "dp.adapter.execute.";
 	public static ExecuteInterface[] initialize(){
 	
-		ExecuteInterface[] array = new ExecuteInterface[4];
+		ExecuteInterface[] array = new ExecuteInterface[3];
 		array[0] = new ExecuteAdapter(EXEC_PACKAGE + "Chris", "goJogging");
 		array[1] = new ExecuteAdapter(EXEC_PACKAGE + "Amby", "playPAD");
 		array[2] = new ExecuteAdapter(EXEC_PACKAGE + "Sammi", "hideInHome");
-		array[3] = new ExecuteAdapter(EXEC_PACKAGE + "Hijack", "playDrum");
 		return array;
 	}
 	
