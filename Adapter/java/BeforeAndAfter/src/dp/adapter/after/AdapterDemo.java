84fe624ed400047cfd34335fae41583850f1b818
Chris Wong <wc38.wong@gmail.com>
2014-01-23 12:15:54 +0800

    Comment

--- Adapter/java/BeforeAndAfter/src/dp/adapter/after/AdapterDemo.java
+++ Adapter/java/BeforeAndAfter/src/dp/adapter/after/AdapterDemo.java
@@ -1,5 +1,7 @@
 package dp.adapter.after;
 
+// The Adapter's "extra level of indirection" takes care of mapping a user-friendly common interface to 
+// legacy-specific peculiar interfaces. 
 public class AdapterDemo{
 	public static void main(String[] args){
 		Shape[] shapes = 
